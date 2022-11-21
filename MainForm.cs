0b5ca92e7032e351cf80faa65e827aae0cd8e522
Dario <dariomrkonjic1@gmail.com>
2022-11-21 19:02:02 +0100

    Add project files.

---
+++ MainForm.cs
@@ -0,0 +1,10 @@
+namespace WinForms_Expense_Manager
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
