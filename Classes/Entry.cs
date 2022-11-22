98eb5ad6d99ba697009a73fb04f1ccc2c5476659
Dario <dariomrkonjic1@gmail.com>
2022-11-22 13:13:54 +0100

    Written Entry class.

---
+++ Classes/Entry.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WinForms_Expense_Manager.Classes
+{
+    /// <summary>
+    /// Represents a single income / expense entry.
+    /// </summary>
+    public class Entry
+    {
+        #region Properties
+        public Guid Id { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Value { get; set; }
+        public Guid CategoryId { get; set; }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Creates a new Entry object.
+        /// </summary>
+        public Entry(string title = "", string description = "", decimal value = 0m)
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = DateTime.UtcNow;
+            Title = title;
+            Description = description;
+            Value = value;
+            CategoryId = Guid.Empty;
+        }
+        /// <summary>
+        /// Creates an Entry object.
+        /// </summary>
+        [JsonConstructor]
+        public Entry(Guid id, DateTime createdAt, string title, string description, decimal value, Guid categoryId)
+        {
+            Id=id;
+            CreatedAt=createdAt;
+            Title=title;
+            Description=description;
+            Value=value;
+            CategoryId=categoryId;
+        }
+        #endregion
+    }
+}
--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,10 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(496, 985);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(512, 1024);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(512, 1024);
+            this.Name = "MainForm";
+            this.Text = "Expense Manager";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
